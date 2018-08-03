df9dc00964a4f0d366c0bda7870b93778292d756
miriam007 <miriamu88@gmail.com>
2018-08-02 19:34:04 -0500

    first commit

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+'use strict';
+const arr= ['Bolt', 'Phelps', 'Shawn White', 'Lewis', 'You!'];
+const printListOfOlympians=(array)=> {
+    array.forEach((item)=> {
+        console.log(item);
+    })
+}
+
+printListOfOlympians(arr)
