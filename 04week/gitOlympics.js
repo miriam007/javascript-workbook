8b082d8b03472a930c4a2c8792825efe5a38e8e3
miriam007 <miriamu88@gmail.com>
2018-08-02 19:35:26 -0500

    second comment

--- 04week/gitOlympics.js
+++ 04week/gitOlympics.js
@@ -1,9 +1,10 @@
 'use strict';
-const arr= ['Bolt', 'Phelps', 'Shawn White', 'Lewis', 'You!'];
+
+const arr1= ['Bolt', 'Phelps', 'Shawn White', 'Lewis', 'You!'];
 const printListOfOlympians=(array)=> {
     array.forEach((item)=> {
         console.log(item);
     })
 }
 
-printListOfOlympians(arr)
+printListOfOlympians(arr1)
