e016ed51edb2930cc30436d165d8f263ff31cd4b
miriam007 <miriamu88@gmail.com>
2018-08-04 11:37:15 -0500

    code plan for towers of Hanoi started

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -18,17 +18,22 @@ function printStacks() {
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
-
+//Make the move-If move is valid, pop() to remove it where it's from. Then push() to add it to the end of the row. Maybe even use splice to move it? Would it be bad to change the array in this case permanently?
+//Choose a number to move. iUse access an array item function to the arrayName[0] to move the numbers from the rows
 function movePiece() {
   // Your code here
 
 }
-
+//Move valid?-Use index of the array to write an if statement about valid moves? Or maybe it needs to be an object with a value attached to the numbers inside of the array?
 function isLegal() {
   // Your code here
 
 }
 
+
+
+//Check for win -You'll use the length - 1 to first target the correct array inside the array. Make const for the different lengths to not alter the original array.
+//The second part of the arrays video loops through arrays to see if they match. Use this to check for win. Use the. Array.forEach(item, index) function.
 function checkForWin() {
   // Your code here
 
