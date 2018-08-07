6223ef23711152809eaf954c32b48964e26dcd4d
miriam007 <miriamu88@gmail.com>
2018-08-06 22:41:50 -0500

    checkwin function works, and reset button briefly worked, then I broke it

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -7,11 +7,16 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 //stacks is an object with arrays
+// const startingPoint = {
+//   a: [4, 3, 2, 1],
+//   b: [],
+//   c: []
+// };
 let stacks = {
-  a: [4, 3, 2, 1],
+  a: [1],
   b: [],
-  c: []
-};
+  c: [4, 3, 2,]
+ };  
 
 const printStacks=()=> {
   console.log("a: " + stacks.a);
@@ -19,16 +24,14 @@ const printStacks=()=> {
   console.log("c: " + stacks.c);
 }
 
-//Reset function- printStacks is already a function that prints out stacks. stacks is the object of arrays. Make a new variable to represent the start of the game layout since the game makes the original variable used change.
-const resetGame=(startingPoint)=> {
-  const startingPoint = {
+
+//Reset function- reset the variable back to the startingPoint
+const resetGame=()=> {
+  let stacks = {
     a: [4, 3, 2, 1],
     b: [],
     c: []
   };
-  if (checkForWin === true) {
-    return printStacks(startingPoint;
-  }
 }
 
 
@@ -37,20 +40,15 @@ const resetGame=(startingPoint)=> {
 const movePiece=(startStack, endStack)=> {
   let startPlace=stacks[startStack];
   let endPlace=stacks[endStack];
- 
-  
   if (isLegal(startPlace[startPlace.length-1], endPlace[endPlace.length-1])) {
     const startValue = startPlace.pop();
     return endPlace.push(startValue);
-  } 
-  
-  
+  }  
 }
 //Move valid?-Input is the numbers pulled from the startValue and endValue. Compare them. When the array is empty, it is undefines, which must be included as an option. It returns boolean values
 //The test wants to use input for isLegal that target the array name from the stacks object, which equals a letter. How can I refactor this to allow for that input?
 const isLegal=(startPlace, endPlace)=> {
- 
-  if(endPlace===undefined || startPlace < endPlace){
+  if (endPlace===undefined || startPlace < endPlace){
     return true;
   } else {
     return false;
@@ -59,22 +57,25 @@ const isLegal=(startPlace, endPlace)=> {
 
 
 //A win happens when either all the blocks are in the correct order in the B or C array. Since the isLegal function prevents moves that aren't allowed, only when there is a win will all blocks be found in the B or C array. So you can use length to test for the win. The length for the B or C stack should equal 4. 
-const checkForWin=() => {
-
+const checkForWin=(stacks) => {
   if ((stacks.b.length === 4) || (stacks.c.length === 4)) {
-    return 'You win!';
+    return true;
+  } else {
+    return false;
   }
 }
-
 
 const towersOfHanoi=(startStack, endStack)=> {
-  if (checkForWin === true) {
-    resetGame(startingPoint);
-    return 'You win';
-  } else {
+  if (isLegal(startStack, endStack)) {
     movePiece(startStack, endStack);
+    if (checkForWin(stacks)) {
+      resetGame();
+      return console.log('You won!!! Play again?');
+    }
+  } else {
+    return console.log('Invalid move. Try again.');
   }
-}
+};
 
 function getPrompt() {
   printStacks();
