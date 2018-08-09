f47140be3b293e3afa48792263fa17b5e5af9cb4
miriam007 <miriamu88@gmail.com>
2018-08-09 16:19:17 -0500

    changed the start stacks back to the correct starting place

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -14,9 +14,9 @@ let startingPoint = {
 };
 
 let stacks = {
-  a: [1],
+  a: [4,3,2,1],
   b: [],
-  c: [4, 3, 2,]
+  c: []
  };  
 
  
@@ -42,7 +42,7 @@ const movePiece=(startStack, endStack)=> {
     const startValue = startPlace.pop();
     return endPlace.push(startValue);
   }  
-}
+};
 //Move valid?-Input is the numbers pulled from the startValue and endValue. Compare them. When the array is empty, it is undefines, which must be included as an option. It returns boolean values
 //The test wants to use input for isLegal that target the array name from the stacks object, which equals a letter. How can I refactor this to allow for that input?
 const isLegal=(startPlace, endPlace)=> {
@@ -51,7 +51,7 @@ const isLegal=(startPlace, endPlace)=> {
   } else {
     return false;
   } 
-}
+};
 
 
 //A win happens when either all the blocks are in the correct order in the B or C array. Since the isLegal function prevents moves that aren't allowed, only when there is a win will all blocks be found in the B or C array. So you can use length to test for the win. The length for the B or C stack should equal 4. 
@@ -61,17 +61,17 @@ const checkForWin=(stacks) => {
   } else {
     return false;
   }
-}
+};
 
 const towersOfHanoi=(startStack, endStack)=> {
   if (isLegal(startStack, endStack)) {
     movePiece(startStack, endStack);
     if (checkForWin(stacks)) {
       resetGame(stacks);
-      return console.log('You won!!! Play again?');
+      console.log('You won!!! Play again?');
     }
   } else {
-    return console.log('Invalid move. Try again.');
+    console.log('Invalid move. Try again.');
   }
 };
 
