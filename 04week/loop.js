92bf47318f4f4ea99f203111bea2091a6aca93bf
miriam007 <miriamu88@gmail.com>
2018-08-11 21:29:09 -0500

    loop assignment finishing the questions

--- 04week/loop.js
+++ 04week/loop.js
@@ -43,7 +43,13 @@ do {
 // When is a for loop better than a while loop?
 //We know how many iterators we are going through with a for loop. In a while loop, you don't know how many iterators you need. 
 // How is the readability of the code affected? 
-// In a for loop, all three parts (initializer, test condition, and updater) are written together in a single line (called an iteration statement), in a while, they're scattered and lie at different places. This makes a for-loop more readable than a while-loop and as a result, more easily maintainable.
+// In a for loop, all three parts (initializer, test condition, and updater) are written together in a single line (called an iteration statement), in a while, they're scattered at different places. This makes a for-loop more readable than a while-loop.
 // What is the difference between a for loop and a for...in loop?
-
+ //A for...in loop loops through the properties of an object. 
+//  for (variable in object) { ...
+//  }
+ //A for loop loops through a block of code numerous times. It is better to use a for loop for arrays where index order is important
+ //for (i=0; i < array.length; i++) {...
+ //}
 // What is the difference between a while loop and a do...while loop?
+//A do...while loop loops through a block of code first without checking if the condition is true. Then it keeps looping though as long as the condition is met. A while loop loops through a block of code when the condition is true. It stops running when the condition is false.
